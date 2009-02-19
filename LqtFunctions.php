f3c5924c66b55471dd7807a2e88c128c773b0eed
Aaron Schulz <aaron@users.mediawiki.org>
2009-02-19 17:20:19 +0000

    Add comment

--- LqtFunctions.php
+++ LqtFunctions.php
@@ -2,13 +2,15 @@
 if ( !defined( 'MEDIAWIKI' ) )
 	die();
 
+/*
+* Get a value from a global array if it exists, otherwise use $default
+*/
 function efArrayDefault( $name, $key, $default ) {
 	global $$name;
 	if ( isset( $$name ) && is_array( $$name ) && array_key_exists( $key, $$name ) ) {
 		$foo = $$name;
 		return $foo[$key];
-	}
-	else {
+	} else {
 		return $default;
 	}
 }
