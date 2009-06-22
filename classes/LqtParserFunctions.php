72053ce6dc8a88e76ff87f88e9959cfcb7ebf88d
Andrew Garrett <werdna@users.mediawiki.org>
2009-06-22 15:18:31 +0000

    E_STRICT

--- classes/LqtParserFunctions.php
+++ classes/LqtParserFunctions.php
@@ -1,7 +1,7 @@
 <?php
 
 class LqtParserFunctions {
-	function archivestartdays( &$parser, $param1 ) {
+	static function archivestartdays( &$parser, $param1 ) {
 		$parser->mOutput->setProperty( 'lqt-archivestartdays', $param1 );
 	}
 }
