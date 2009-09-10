215211d8e44a1ef4d0f8a98aa6ae5cf0eedaddb2
Andrew Garrett <werdna@users.mediawiki.org>
2009-09-10 21:51:10 +0000

    Fix notice

--- pages/SpecialMergeThread.php
+++ pages/SpecialMergeThread.php
@@ -128,7 +128,7 @@ class SpecialMergeThread extends ThreadActionPage {
 		$thread->save();
 		
 		foreach( $thread->replies() as $subThread ) {
-			$this->recursiveSet( $subThread, $subject, $ancestor, $reason );
+			$this->recursiveSet( $subThread, $subject, $ancestor );
 		}
 	}
 	
