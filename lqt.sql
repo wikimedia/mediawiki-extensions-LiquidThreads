8ec3922318bbeeebc5a78a7f6e7f0f6742d87f28
Andrew Garrett <werdna@users.mediawiki.org>
2010-06-30 17:59:05 +0000

    Shorten tpr_title column

--- lqt.sql
+++ lqt.sql
@@ -91,7 +91,7 @@ CREATE TABLE /*_*/thread_history (
 CREATE TABLE /*_*/thread_pending_relationship (
 	tpr_thread int unsigned NOT NULL,
 	tpr_relationship varbinary(64) NOT NULL,
-	tpr_title varbinary(1024) NOT NULL,
+	tpr_title varbinary(255) NOT NULL,
 	tpr_type varbinary(32) NOT NULL,
 	PRIMARY KEY (tpr_thread,tpr_relationship)
 ) /*$wgDBTableOptions*/;
