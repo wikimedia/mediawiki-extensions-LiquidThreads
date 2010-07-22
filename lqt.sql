f79af1ce4b35f3a3823925505b3869b4d25812af
Mark A. Hershberger <mah@users.mediawiki.org>
2010-07-22 00:08:10 +0000

    Adjust SQL so that it works in SQLite as well as MySQL

--- lqt.sql
+++ lqt.sql
@@ -1,5 +1,5 @@
 CREATE TABLE /*$wgDBprefix*/thread (
-  thread_id int(8) unsigned NOT NULL auto_increment,
+  thread_id int(8) unsigned NOT NULL PRIMARY KEY auto_increment,
   thread_root int(8) unsigned UNIQUE NOT NULL,
   thread_ancestor int(8) unsigned NOT NULL,
   thread_parent int(8) unsigned NULL,
@@ -19,28 +19,26 @@ CREATE TABLE /*$wgDBprefix*/thread (
 
   -- Special thread types (deleted/move trace/normal)
   thread_type int(4) unsigned NOT NULL default 0,
-  
+
   -- Sort key
   thread_sortkey varchar(255) NOT NULL default '',
-  
+
   -- Reply count, -1 means uninitialised.
   thread_replies int(8) DEFAULT -1,
-  
+
   -- Signature
   thread_signature TINYBLOB NULL,
 
-  PRIMARY KEY thread_id (thread_id),
-  UNIQUE INDEX thread_root_page (thread_root),
-  INDEX thread_ancestor (thread_ancestor, thread_parent),
-  INDEX thread_article_title (thread_article_namespace, thread_article_title, thread_sortkey),
-  INDEX thread_article (thread_article_id, thread_sortkey),
-  INDEX thread_modified (thread_modified),
-  INDEX thread_created (thread_created),
-  INDEX thread_summary_page (thread_summary_page),
-  INDEX (thread_author_id,thread_author_name),
-  INDEX (thread_sortkey)
+  UNIQUE (thread_root)
 ) /*$wgDBTableOptions*/;
-
+CREATE INDEX thread_ancestor ON /*$wgDBprefix*/thread (thread_ancestor, thread_parent);
+CREATE INDEX thread_article_title ON /*$wgDBprefix*/thread (thread_article_namespace, thread_article_title, thread_sortkey);
+CREATE INDEX thread_article ON /*$wgDBprefix*/thread (thread_article_id, thread_sortkey);
+CREATE INDEX thread_modified ON /*$wgDBprefix*/thread (thread_modified);
+CREATE INDEX thread_created ON /*$wgDBprefix*/thread (thread_created);
+CREATE INDEX thread_summary_page ON /*$wgDBprefix*/thread (thread_summary_page);
+CREATE INDEX thread_author_name ON /*$wgDBprefix*/thread (thread_author_id,thread_author_name);
+CREATE INDEX thread_sortkey ON /*$wgDBprefix*/thread (thread_sortkey);
 
 -- Old storage table for "historical" (i.e. non-current) threads
 -- Now superseded by thread_history.
@@ -52,7 +50,8 @@ CREATE TABLE /*$wgDBprefix*/historical_thread (
   hthread_contents BLOB NOT NULL,
   hthread_change_type int(4) unsigned NOT NULL,
   hthread_change_object int(8) unsigned NULL,
-  PRIMARY KEY hthread_id_revision (hthread_id, hthread_revision)
+
+  PRIMARY KEY (hthread_id, hthread_revision) 
 ) /*$wgDBTableOptions*/;
 
 CREATE TABLE /*$wgDBprefix*/user_message_state (
@@ -60,32 +59,30 @@ CREATE TABLE /*$wgDBprefix*/user_message_state (
   ums_thread int(8) unsigned NOT NULL,
   ums_read_timestamp varbinary(14),
 
-  PRIMARY KEY (ums_user, ums_thread),
-  KEY (ums_user,ums_read_timestamp)
+  PRIMARY KEY (ums_user, ums_thread)
 ) /*$wgDBTableOptions*/;
+CREATE INDEX ums_user_read ON /*$wgDBprefix*/user_message_state (ums_user,ums_read_timestamp);
 
 -- "New" storage location for history data.
 CREATE TABLE /*_*/thread_history (
-	th_id int unsigned NOT NULL auto_increment,
+	th_id int unsigned NOT NULL PRIMARY KEY auto_increment,
 	th_thread int unsigned NOT NULL,
-	
+
 	th_timestamp varbinary(14) NOT NULL,
-	
+
 	th_user int unsigned NOT NULL,
 	th_user_text varchar(255) NOT NULL,
-	
+
 	th_change_type int unsigned NOT NULL,
 	th_change_object int unsigned NOT NULL,
 	th_change_comment TINYTEXT NOT NULL,
-	
+
 	-- Actual content, stored as a serialised thread row.
-	th_content LONGBLOB NOT NULL,
-	
-	PRIMARY KEY (th_id),
-	KEY (th_thread,th_timestamp),
-	KEY (th_timestamp,th_thread),
-	KEY (th_user,th_user_text)
+	th_content LONGBLOB NOT NULL
 ) /*$wgDBTableOptions*/;
+CREATE INDEX th_thread_timestamp ON /*$wgDBprefix*/thread_history (th_thread,th_timestamp);
+CREATE INDEX th_timestamp_thread ON /*$wgDBprefix*/thread_history (th_timestamp,th_thread);
+CREATE INDEX th_user_text ON /*$wgDBprefix*/thread_history (th_user,th_user_text);
 
 -- Storage for "pending" relationships from import
 CREATE TABLE /*_*/thread_pending_relationship (
@@ -103,7 +100,7 @@ CREATE TABLE /*_*/thread_reaction (
 	tr_user_text varbinary(255) NOT NULL,
 	tr_type varbinary(64) NOT NULL,
 	tr_value int NOT NULL,
-	
-	PRIMARY KEY (tr_thread,tr_user,tr_user_text,tr_type,tr_value),
-	KEY (tr_user,tr_user_text,tr_type,tr_value)
+
+	PRIMARY KEY (tr_thread,tr_user,tr_user_text,tr_type,tr_value)
 ) /*$wgDBTableOptions*/;
+CREATE INDEX tr_user_text_value ON /*$wgDBprefix*/thread_reaction (tr_user,tr_user_text,tr_type,tr_value);
