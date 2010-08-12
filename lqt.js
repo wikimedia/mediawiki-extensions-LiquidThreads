91dbd68282309daa913ebaac7c6c91262c14fa7e
Adam Miller <adam@users.mediawiki.org>
2010-08-12 00:29:37 +0000

    more improvements (i hope) to the placement of dropzones when moving a thread

--- lqt.js
+++ lqt.js
@@ -1168,6 +1168,10 @@ var liquidThreads = {
 		// Now one underneath every thread except the drag thread
 		$j('.lqt_thread').not( $thread ).each( function() {
 			var $curThread = $j( this );
+			// don't put any drop zones under child threads
+			if ( $j.contains( $thread[0], $curThread[0] ) ) return;
+			// don't put it right next to the thread
+			if ( $curThread.find( '.lqt-thread-replies:first > .lqt_thread:last' )[0] == $thread[0] ) return;
 			var repliesElement = liquidThreads.getRepliesElement( $curThread );
 			repliesElement.contents().filter('.lqt-replies-finish').before( createDropZone( 'now', $curThread.data( 'thread-id' ) ) );
 		} );
