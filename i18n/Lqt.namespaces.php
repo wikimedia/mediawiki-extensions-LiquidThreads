689b6738b35fbf701dd5c158d5d73ffcfecf7218
Andrew Garrett <werdna@users.mediawiki.org>
2010-08-17 10:23:20 +0000

    Add namespaces in pt

--- i18n/Lqt.namespaces.php
+++ i18n/Lqt.namespaces.php
@@ -15,3 +15,10 @@ $namespaceNames['fi'] = array(
 	NS_LQT_SUMMARY      => 'Yhteenveto',
 	NS_LQT_SUMMARY_TALK => 'Keskustelu_yhteenvedosta',
 );
+
+$namespaceNames['pt'] = array(
+	NS_LQT_THREAD => 'Tópico',
+	NS_LQT_THREAD_TALK => 'Tópico_discussão',
+	NS_LQT_SUMMARY => 'Resumo',
+	NS_LQT_SUMMARY_TALK => 'Resumo_discussão',
+);
