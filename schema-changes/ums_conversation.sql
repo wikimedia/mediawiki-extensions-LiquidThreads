855bccf9605c531d0f74e4d1635120a69a144cd2
Niklas Laxström <nikerabbit@users.mediawiki.org>
2010-09-11 14:51:57 +0000

    Fix half-working schema change patch

--- schema-changes/ums_conversation.sql
+++ schema-changes/ums_conversation.sql
@@ -2,9 +2,9 @@
 -- Adds and populates the ums_conversation field, along with relevant indices.
 ALTER TABLE /*_*/user_message_state ADD COLUMN ums_conversation int(8) unsigned NOT NULL DEFAULT 0;
 
-CREATE INDEX /*i*/ums_user_conversation ON /*$wgDBprefix*/user_message_state (ums_user,ums_conversation);
+CREATE INDEX /*i*/ums_user_conversation ON /*_*/user_message_state (ums_user,ums_conversation);
 DROP INDEX /*i*/ums_user_conversation ON /*_*/user_message_state;
 
 UPDATE /*_*/user_message_state, /*_*/thread
-SET user_message_state.ums_conversation = if(thread.thread_ancestor, thread.thread_ancestor, thread.thread_id)
-WHERE ums_conversation = 0 AND thread.thread_id = user_message_state.ums_thread;
+SET /*_*/user_message_state.ums_conversation = if(/*_*/thread.thread_ancestor, /*_*/thread.thread_ancestor, /*_*/thread.thread_id)
+WHERE ums_conversation = 0 AND /*_*/thread.thread_id = /*_*/user_message_state.ums_thread;
