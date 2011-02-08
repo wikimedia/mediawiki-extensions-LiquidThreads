9230daf9ed99a4c356e7577da9d940eaf75cc7d1
Platonides <platonides@users.mediawiki.org>
2011-02-08 15:05:37 +0000

    Quick fix instead of r81738. This way of working is completely unsupported...

--- classes/View.php
+++ classes/View.php
@@ -2125,9 +2125,9 @@ class LqtView {
 		}
 
 		// Parser gets antsy about parser options here if it hasn't parsed anything before.
+		$wgParser->mOptions = new ParserOptions;
 		$wgParser->clearState();
 		$wgParser->setTitle( $wgTitle );
-		$wgParser->mOptions = new ParserOptions;
 
 		$sig = $wgOut->parseInline( $sig );
 
@@ -2142,9 +2142,9 @@ class LqtView {
 		$title = $wgTitle ? $wgTitle : $user->getUserPage();
 
 		// Parser gets antsy about parser options here if it hasn't parsed anything before.
+		$wgParser->mOptions = new ParserOptions;
 		$wgParser->clearState();
 		$wgParser->setTitle( $title );
-		$wgParser->mOptions = new ParserOptions;
 
 		$sig = $wgParser->preSaveTransform(
 			$sig,
