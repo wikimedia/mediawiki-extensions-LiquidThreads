8e24f2fa2799bb392340ad4102ad627851f5edc6
Leo Koppelkamm <diebuche@users.mediawiki.org>
2011-04-30 18:58:17 +0000

    Followup to r86108 per CR: Fix two calls to legacy tablesorter

--- js/lqt.toolbar.js
+++ js/lqt.toolbar.js
@@ -1572,11 +1572,14 @@ liquidThreads.toolbar = {
 					<span rel="wikieditor-toolbar-tool-table-example"></span>\
 					<div class="wikieditor-toolbar-table-preview-content">\
 						<table id="wikieditor-toolbar-table-preview" class="wikieditor-toolbar-table-preview wikitable">\
+						<thead>\
 							<tr class="wikieditor-toolbar-table-preview-header">\
 								<th rel="wikieditor-toolbar-tool-table-example-header"></th>\
 								<th rel="wikieditor-toolbar-tool-table-example-header"></th>\
 								<th rel="wikieditor-toolbar-tool-table-example-header"></th>\
-							</tr><tr class="wikieditor-toolbar-table-preview-hidden" style="display: none;">\
+							</tr>\
+						</thead><tbody>\
+							<tr class="wikieditor-toolbar-table-preview-hidden" style="display: none;">\
 								<td rel="wikieditor-toolbar-tool-table-example-cell-text"></td>\
 								<td rel="wikieditor-toolbar-tool-table-example-cell-text"></td>\
 								<td rel="wikieditor-toolbar-tool-table-example-cell-text"></td>\
@@ -1593,6 +1596,7 @@ liquidThreads.toolbar = {
 								<td rel="wikieditor-toolbar-tool-table-example-cell-text"></td>\
 								<td rel="wikieditor-toolbar-tool-table-example-cell-text"></td>\
 							</tr>\
+						</tbody>\
 						</table>\
 					</div>\
 				</div></div>',
@@ -1611,15 +1615,14 @@ liquidThreads.toolbar = {
 
 				// Hack for sortable preview: dynamically adding
 				// sortable class doesn't work, so we use a clone
-				// FIXME: Relies on sortable table internals
 				$j( '#wikieditor-toolbar-table-preview' )
 					.clone()
 					.attr( 'id', 'wikieditor-toolbar-table-preview2' )
 					.addClass( 'sortable' )
 					.insertAfter( $j( '#wikieditor-toolbar-table-preview' ) )
 					.hide();
-				if ( typeof ts_makeSortable == 'function' )
-					ts_makeSortable( $j( '#wikieditor-toolbar-table-preview2' ).get( 0 ) );
+				if ( typeof jQuery.fn.tablesorter == 'function' )
+						$j( '#wikieditor-toolbar-table-preview2' ).tablesorter();
 				$j( '#wikieditor-toolbar-table-sortable' ).click( function() {
 					// Swap the currently shown one clone with the other one
 					$j( '#wikieditor-toolbar-table-preview' )
@@ -1639,12 +1642,11 @@ liquidThreads.toolbar = {
 					var hiddenHTML = $j( '.wikieditor-toolbar-table-preview-hidden' ).html();
 					$j( '.wikieditor-toolbar-table-preview-header' ).html( hiddenHTML );
 					$j( '.wikieditor-toolbar-table-preview-hidden' ).html( headerHTML );
-					if ( typeof ts_makeSortable == 'function' )
-						ts_makeSortable(
-	     						$j( '#wikieditor-toolbar-table-preview, #wikieditor-toolbar-table-preview2' )
+					if ( typeof jQuery.fn.tablesorter == 'function' ) {
+							$( '#wikieditor-toolbar-table-preview, #wikieditor-toolbar-table-preview2' )
 								.filter( '.sortable' )
-								.get( 0 )
-						);
+								.tablesorter();
+					}
 				});
 
 			},
