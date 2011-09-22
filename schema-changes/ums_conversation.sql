af8b2bc22afddb81098eb621b5c32753dba4ad5b
Sam Reed <reedy@users.mediawiki.org>
2011-09-22 20:24:12 +0000

    LQT schema seems to be a sham

--- schema-changes/ums_conversation.sql
+++ schema-changes/ums_conversation.sql
@@ -3,7 +3,7 @@
 ALTER TABLE /*_*/user_message_state ADD COLUMN ums_conversation int(8) unsigned NOT NULL DEFAULT 0;
 
 CREATE INDEX /*i*/ums_user_conversation ON /*_*/user_message_state (ums_user,ums_conversation);
-DROP INDEX /*i*/ums_read ON /*_*/user_message_state;
+DROP INDEX /*i*/ums_user_read ON /*_*/user_message_state;
 
 UPDATE /*_*/user_message_state, /*_*/thread
 SET /*_*/user_message_state.ums_conversation = if(/*_*/thread.thread_ancestor, /*_*/thread.thread_ancestor, /*_*/thread.thread_id)
