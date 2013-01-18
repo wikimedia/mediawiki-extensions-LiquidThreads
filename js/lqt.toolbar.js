bfa0bb9eb224a598e0fa602c7a8baf90335bbbd4
Antoine Musso <hashar@free.fr>
2013-01-18 15:53:14 +0100

    JSHint: unescaped square brackets in regex
    Some square brackets (] and [) were not properly escaped when being used
    in regex. They were marked as FIXME entry, so being bold and fix them on
    sight.
    That probably deserve a human test in the web interface.
    
    Change-Id: Iaefe3870b5e5b645fc668e36169b841ef7f48c34

--- js/lqt.toolbar.js
+++ js/lqt.toolbar.js
@@ -1260,17 +1260,15 @@ liquidThreads.toolbar = {
 				buttons: {
 					'wikieditor-toolbar-tool-link-insert': function() {
 						function escapeInternalText( s ) {
-							// FIXME: Should this escape [[ too? Seems to work without that
-							return s.replace( /(]{2,})/g, '<nowiki>$1</nowiki>' );
+							return s.replace( /(\]{2,})/g, '<nowiki>$1</nowiki>' );
 						}
 						function escapeExternalTarget( s ) {
 							return s.replace( / /g, '%20' )
 								.replace( /\[/g, '%5B' )
-								.replace( /]/g, '%5D' );
+								.replace( /\]/g, '%5D' );
 						}
 						function escapeExternalText( s ) {
-							// FIXME: Should this escape [ too? Seems to work without that
-							return s.replace( /(]+)/g, '<nowiki>$1</nowiki>' );
+							return s.replace( /(\]+)/g, '<nowiki>$1</nowiki>' );
 						}
 						var insertText = '',
 							whitespace = $j( '#wikieditor-toolbar-link-dialog' ).data( 'whitespace' ),
