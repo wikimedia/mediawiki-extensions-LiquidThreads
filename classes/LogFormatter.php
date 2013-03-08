b006a0875a3b920f9f7533baaa38a383176c233a
shirayuki <shirayuking@gmail.com>
2013-03-09 06:26:31 +0900

    Adding comments for grep
    Change-Id: I91f0056b07aee7a19c57fc73d095f8ea07effca9

--- classes/LogFormatter.php
+++ classes/LogFormatter.php
@@ -12,6 +12,9 @@ class LqtLogFormatter {
 				}
 				break;
 			default:
+				// Give grep a chance to find the usages:
+				// lqt-log-action-move, lqt-log-action-split, lqt-log-action-subjectedit,
+				// lqt-log-action-resort, lqt-log-action-signatureedit
 				$msg = "lqt-log-action-$action";
 				break;
 		}
