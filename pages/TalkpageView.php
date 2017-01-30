a07b03fa98fecf1aa7b7644f28658941cc28c90a
Reedy <reedy@wikimedia.org>
2017-01-30 21:37:01 +0000

    Sanitise page limit
    Bug: T155265
    Change-Id: I9ed476fe208ea39e9dbdcfa671a8614ac8e7116a
    (cherry picked from commit f1d83d646561bda26e180acf6034f138f7264e00)

--- pages/TalkpageView.php
+++ pages/TalkpageView.php
@@ -432,7 +432,7 @@ class LqtDiscussionPager extends IndexPager {
 
 		parent::__construct();
 
-		$this->mLimit = $this->getPageLimit();
+		$this->setLimit( $this->getPageLimit() );
 	}
 
 	function getPageLimit() {
