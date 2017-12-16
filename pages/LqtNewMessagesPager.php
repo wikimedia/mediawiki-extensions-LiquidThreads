e5b0438f7de9eb6afb2c842a26f1c8049e6bff6b
Umherirrender <umherirrender_de.wp@web.de>
2017-12-16 14:30:53 +0100

    Move classes to own files
    Makes MediaWiki.Files.OneClassPerFile.MultipleFound pass
    
    Change-Id: Iad77a4515588881effaa4f6ef7810a7267dc4ac4

---
+++ pages/LqtNewMessagesPager.php
@@ -0,0 +1,85 @@
+<?php
+
+class LqtNewMessagesPager extends LqtDiscussionPager {
+	private $user;
+
+	function __construct( $user ) {
+		$this->user = $user;
+
+		parent::__construct( false, false );
+	}
+
+	/**
+	 * Returns an array of structures. Each structure has the keys 'top' and 'posts'.
+	 * 'top' contains the top-level thread to display.
+	 * 'posts' contains an array of integer post IDs which should be highlighted.
+	 * @return false|array
+	 */
+	function getThreads() {
+		$rows = $this->getRows();
+
+		if ( ! count( $rows ) ) {
+			return false;
+		}
+
+		$threads = Thread::bulkLoad( $rows );
+		$thread_ids = array_keys( $threads );
+		$output = [];
+
+		foreach ( $threads as $id => $thread ) {
+			$output[$id] = [ 'top' => $thread, 'posts' => [] ];
+		}
+
+		$dbr = wfGetDB( DB_REPLICA );
+
+		$res = $dbr->select( [ 'user_message_state' ],
+					[ 'ums_thread', 'ums_conversation' ],
+					[
+						'ums_user' => $this->user->getId(),
+						'ums_conversation' => $thread_ids
+					],
+					__METHOD__
+					);
+
+		foreach ( $res as $row ) {
+			$top = $row->ums_conversation;
+			$thread = $row->ums_thread;
+			$output[$top]['posts'][] = $thread;
+		}
+
+		return $output;
+	}
+
+	function getQueryInfo() {
+		$dbr = wfGetDB( DB_REPLICA );
+
+		$queryInfo = [
+			'tables' => [ 'thread', 'user_message_state' ],
+			'fields' => [ $dbr->tableName( 'thread' ) . '.*', 'ums_conversation' ],
+			'conds' => [
+				'ums_user' => $this->user->getId(),
+				'thread_type != ' . $this->mDb->addQuotes( Threads::TYPE_DELETED ),
+			],
+			'join_conds' => [
+				'thread' => [ 'join', 'ums_conversation=thread_id' ]
+			],
+			'options' => [
+				'group by' => 'ums_conversation'
+			]
+		];
+
+		return $queryInfo;
+	}
+
+	function getPageLimit() {
+		return 25;
+	}
+
+	function getDefaultDirections() {
+		return true; // Descending
+	}
+
+	function getIndexField() {
+		return [ 'ums_conversation' ];
+	}
+}
---
+++ pages/TalkpageHistoryPager.php
@@ -0,0 +1,84 @@
+<?php
+
+class TalkpageHistoryPager extends ThreadHistoryPager {
+	function __construct( $view, $talkpage ) {
+		$this->talkpage = $talkpage;
+
+		parent::__construct( $view, null );
+	}
+
+	function getFieldMessages() {
+		$headers = [
+			'th_timestamp' => $this->msg( 'lqt-history-time' )->text(),
+			'thread_subject' => $this->msg( 'lqt-history-thread' )->text(),
+			'th_user_text' => $this->msg( 'lqt-history-user' )->text(),
+			'th_change_type' => $this->msg( 'lqt-history-action' )->text(),
+			'th_change_comment' => $this->msg( 'lqt-history-comment' )->text(),
+			];
+
+		return $headers;
+	}
+
+	function getQueryInfo() {
+		$queryInfo = [
+			'tables' => [ 'thread_history', 'thread', 'page' ],
+			'fields' => '*',
+			'conds' => [ Threads::articleClause( $this->talkpage ) ],
+			'options' => [ 'order by' => 'th_timestamp desc' ],
+			'join_conds' => [
+				'thread' => [ 'LEFT JOIN', 'thread_id=th_thread' ],
+				'page' => [ 'LEFT JOIN', 'thread_root=page_id' ],
+			],
+		];
+
+		return $queryInfo;
+	}
+
+	function formatValue( $name, $value ) {
+		global $wgLang, $wgContLang, $wgOut;
+
+		$wgOut->setRobotPolicy( 'noindex, nofollow' );
+
+		$row = $this->mCurrentRow;
+
+		$ns = $row->page_namespace;
+		$title = $row->page_title;
+
+		if ( is_null( $ns ) ) {
+			$ns = $row->thread_article_namespace;
+			$title = $row->thread_article_title;
+		}
+
+		switch ( $name ) {
+			case 'thread_subject':
+				$title = Title::makeTitleSafe(
+					$ns,
+					$title
+				);
+
+				$link = $this->linkRenderer->makeKnownLink(
+					$title,
+					$value,
+					[],
+					[]
+				);
+
+				return Html::rawElement( 'div', [ 'dir' => $wgContLang->getDir() ], $link );
+			case 'th_timestamp':
+				$formatted = $wgLang->timeanddate( $value );
+				$title = Title::makeTitleSafe(
+					$ns,
+					$title
+				);
+
+				return $this->linkRenderer->makeLink(
+					$title,
+					$formatted,
+					[],
+					[ 'lqt_oldid' => $row->th_id ]
+				);
+			default:
+				return parent::formatValue( $name, $value );
+		}
+	}
+}
