wikimedia/mediawiki-extensions-LiquidThreads

commit 92e7763f5f790ce38dadf102e0fa298d9429c29e

LiquidThreads: Add a preference (on by default) as to whether or not to display custom user signatures on posts. If it is off, it will just use the default signature. An end to the signature wars :).

classes/View.php
@@ -29,6 +29,7 @@ class LqtView {
 	static $stylesAndScriptsDone = false;
 	
 	static $userSignatureCache = array();
+	static $boringSignatureCache = array();
 
 	function __construct( &$output, &$article, &$title, &$user, &$request ) {
 		$this->article = $article;
@@ -1217,7 +1218,29 @@ class LqtView {
 			$name = $user->getName();
 			$uid = $user->getId();
 		}
+	
+		if ( $this->user->getOption( 'lqtcustomsignatures' ) ) {
+			return $this->getUserSignature( $user, $uid, $name );
+		} else {
+			return $this->getBoringSignature( $user, $uid, $name );
+		}
+	}
+	
+	function getBoringSignature( $user, $uid, $name ) {
+		if ( isset( self::$boringSignatureCache[$name] ) ) {
+			return self::$boringSignatureCache[$name];
+		}
 		
+		$msg = ($uid > 0) ? 'signature' : 'signature-anon';
+		
+		$sig = wfMsgExt( $msg, 'parseinline', array($name, $name) );
+		
+		self::$boringSignatureCache[$name] = $sig;
+		
+		return $sig;
+	}
+	
+	function getUserSignature( $user, $uid, $name ) {
 		if ( isset( self::$userSignatureCache[$name] ) ) {
 			return self::$userSignatureCache[$name];
 		}

LiquidThreads.php
@@ -140,6 +140,7 @@ $wgLogActionsHandlers['liquidthreads/move'] = 'lqtFormatMoveLogEntry';
 $wgDefaultUserOptions['lqtnotifytalk'] = true;
 $wgDefaultUserOptions['lqtdisplaydepth'] = 3;
 $wgDefaultUserOptions['lqtdisplaycount'] = 10;
+$wgDefaultUserOptions['lqtcustomsignatures'] = true;
 
 // API
 $wgAutoloadClasses['ApiQueryLQTThreads'] = "$dir/api/ApiQueryLQTThreads.php";

classes/Hooks.php
@@ -151,6 +151,14 @@ class LqtHooks {
 		global $wgEnableEmail;
 		wfLoadExtensionMessages( 'LiquidThreads' );
 		
+		// Whether or not to show user signatures
+		$preferences['lqtcustomsignatures'] =
+			array(
+				'type' => 'toggle',
+				'label-message' => 'lqt-preference-custom-signatures',
+				'section' => 'lqt',
+			);
+		
 		if ($wgEnableEmail) {
 			$preferences['lqtnotifytalk'] =
 				array(
@@ -176,7 +184,6 @@ class LqtHooks {
 				'section' => 'lqt',
 			);
 			
-		// Display depth and count
 		$preferences['lqtdisplaycount'] =
 			array(
 				'type' => 'int',

i18n/Lqt.i18n.php
@@ -213,6 +213,7 @@ To avoid this, split them from this thread before deleting it.',
 	'prefs-lqt' => 'Threaded discussion',
 	'lqt-preference-display-depth' => 'Maximum reply depth to show:',
 	'lqt-preference-display-count' => 'Maximum number of replies to show:',
+	'lqt-preference-custom-signatures' => 'Show custom user signatures',
 
 	// E-mail notification
 	'lqt-enotif-subject-reply' => '{{SITENAME}} discussion - Reply: $1',

commit decfe683d1f78351be83b9f10c543e107bcfb224

Remove View::getBoringSignature() call broken since 2009
The method getBoringSignature() was introduced in commit
92e7763f5f790ce38dadf102e0fa298d9429c29e (2009-10-02) by Andrew Garrett.
The author described the change as follows:

"Add a preference (on by default) as to whether or not to display custom
user signatures on posts. If it is off, it will just use the default
signature. An end to the signature wars :)."

Just a few weeks later in commit cfbcd544b3e7302c289412ca371277f7e23969dd
(2009-12-18) the same author removed the code again, but forgot about
this call. The commit message back then was:

"Make signatures a per-post affair, defaulting to the user's signature,
but editable in the process of editing a post, and indeed variable on a
per-post basis."

Luckily this code path was dead anyway because the user option
"lqtcustomsignatures" defaults to true, and there was no way to change
it.

Change-Id: I3df193f865966b7b76bd2372d6855748c7ced576

classes/View.php
@@ -2338,24 +2338,15 @@ class LqtView {
 	public function getSignature( $user ) {
 		if ( is_object( $user ) ) {
 			$uid = $user->getId();
-			$name = $user->getName();
 		} elseif ( is_int( $user ) ) {
 			$uid = $user;
 			$user = User::newFromId( $uid );
-			$name = $user->getName();
 		} else {
 			$user = User::newFromName( $user );
-			$name = $user->getName();
 			$uid = $user->getId();
 		}
 
-		if ( $this->user->getOption( 'lqtcustomsignatures' ) ) {
-			return $this->getUserSignature( $user, $uid );
-		} else {
-			// @FIXME Method not found
-			// @phan-suppress-next-line PhanUndeclaredMethod
-			return $this->getBoringSignature( $user, $uid, $name );
-		}
+		return $this->getUserSignature( $user, $uid );
 	}
 
 	public static function getUserSignature( $user, $uid = null ) {